37b7f80a64cf3e24d78a004a6239ef855398c729
sieg32 <yogeshmalviya.raptor@gmail.com>
2024-12-16 14:39:40 +0530

    basic

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "jsx", // Ensure JSX is parsed in .js files
-    include: /src\/.*\.js$/, // Apply only to .js files in the src folder
-  }
-})
+});
